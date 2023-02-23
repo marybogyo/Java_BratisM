53f88a0bb0e0dad3bbb4399b48c4875bec47e144
marybogyo <bratis.marianna@gmail.com>
2023-02-23 18:06:38 +0100

    metodus_gyak

---
+++ Metodus_gyakorlas.java
@@ -0,0 +1,33 @@
+
+package metodus_gyakorlas;
+
+public class Metodus_gyakorlas {
+
+    public static void main(String[] args) {
+        int osszeg = elso10SzamOsszeg();
+        String kimenet = "Az első 10szám összege: " + osszeg + "\n";
+        Kiir(kimenet);
+        
+        int szam1 = 7, szam2 = 4;
+        Kiir("%d + %d = %d\n".formatted(szam1,szam2,szam1+szam2));
+    }
+  
+        public static int elso10SzamOsszeg(){
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += i;
+        }
+        return osszeg
+                
+        //Kiir("Az első 10szám összege: " + osszeg);
+    }
+    public static void Kiir(String vmi)
+    {
+        System.out.println(vmi);
+    }
+    private static void osszead(int a, int b){
+        return a+b;
+     /*System.out.printf("%d + %d = %d\n", a, b, a+b);*/
+    }
+}
+
